8e8af6b77603003009c3e6e4aa8c1a7e69019a19
harshahir01 <yadavharsh2324@gmail.com>
2024-01-17 09:52:18 -0500

    edit

--- calculate_square.py
+++ calculate_square.py
@@ -1,3 +1,7 @@
-num1=input("Enter the number 1")
-num2=input("Enter the number 2")
-print(num1*num2)
+# num1=input(int("Enter the number 1\n"))
+# num2=input(int("Enter the number 2\n"))
+# print(num1*num2)
+def square(num1,num2):
+    return num1*num2
+print(square(10,20))
+    
